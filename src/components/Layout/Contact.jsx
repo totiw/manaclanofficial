83e457271ac99e960a2c1e5429c2bbf449ae8b33
Toto Jati Wijayanto <97005196+totiw@users.noreply.github.com>
2023-04-14 17:17:09 +0700

    adjust texxt size in contact section

--- src/components/Layout/Contact.jsx
+++ src/components/Layout/Contact.jsx
@@ -14,19 +14,19 @@ const Contact = () => {
       <div className="flex flex-col gap-3 lg:gap-10">
         <div className="flex flex-row items-center gap-3 lg:gap-10">
           <img src={Facebook} alt="facebook icon" className="w-[22px] lg:w-[64px]" />
-          <p className="text-white text-[12px] lg:text-3xl font-bold lg:tracking-[5px]">Manaclanid</p>
+          <p className="text-white text-[12px] lg:text-3xl font-bold lg:tracking-[2px]">Manaclanid</p>
         </div>
         <div className="flex flex-row items-center gap-3 lg:gap-10">
           <img src={Instagram} alt="facebook icon" className="w-[22px] lg:w-[64px]" />
-          <p className="text-white text-[12px] lg:text-3xl font-bold lg:tracking-[5px]">Manaclanid</p>
+          <p className="text-white text-[12px] lg:text-3xl font-bold lg:tracking-[2px]">Manaclanid</p>
         </div>
         <div className="flex flex-row items-center gap-3 lg:gap-10">
           <img src={Youtube} alt="facebook icon" className="w-[22px] lg:w-[64px]" />
-          <p className="text-white text-[12px] lg:text-3xl font-bold lg:tracking-[5px]">Mana Gaming id</p>
+          <p className="text-white text-[12px] lg:text-3xl font-bold lg:tracking-[2px]">Mana Gaming id</p>
         </div>
         <div className="flex flex-row items-center gap-3 lg:gap-10">
           <img src={Discord} alt="facebook icon" className="w-[22px] lg:w-[64px]" />
-          <p className="text-white text-[12px] lg:text-3xl font-bold lg:tracking-[5px]">Mana Gaming id</p>
+          <p className="text-white text-[12px] lg:text-3xl font-bold lg:tracking-[2px]">Mana Gaming id</p>
         </div>
       </div>
       <img src={Console} alt="Mana console" className="w-20 lg:w-96 h-20 lg:h-96 -rotate-12" />
