0d19b85c2462faa995282aa7e00d48ab9a79496d
Toto Jati Wijayanto <97005196+totiw@users.noreply.github.com>
2023-04-19 17:03:29 +0700

    style: resize footer text;

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <img src={Brand} alt="Brand Mana Clan" className="w-24 lg:w-52 2xl:w-80" />
         <span className="w-full h-[10px] lg:h-4 bg-gradient-to-r from-[#EA0A8C] to-[#8ED5E2]"></span>
       </div>
-      <p className="py-7 lg:py-5 2xl:py-9 font-normal text-sm lg:text-xl tracking-[2px]">
+      <p className="py-7 lg:py-5 2xl:py-9 font-normal text-sm 2xl:text-xl tracking-[2px]">
         copyright &copy; 2023 ManaClanId - Alright Reserved
       </p>
     </footer>
